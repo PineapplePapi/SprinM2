64ff4b40426dd4b4f50bf9556d5a261a368799de
Andrea Venegas <andrea.mvng@gmail.com>
2024-02-24 13:22:37 -0300

    #graficosagregados

--- js/control-panel.js
+++ js/control-panel.js
@@ -4,7 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const history = document.getElementById("history");
   const divhistory = document.getElementById("history-toggle");
   const botones = document.querySelectorAll(".boton");
-
+  const btncolesterol = document.getElementById("btncolesterol")
+  const btncolesterol2 = document.getElementById("btncolesterol2")       
+  const btncolesterol3 = document.getElementById("btncolesterol3") 
+  const btntrigliceridos = document.getElementById("btntrigliceridos")
+  let graph = document.getElementById("grafica");
+  let grafico = null;
   history.addEventListener("click", () => {
     divhistory.classList.toggle("d-none");
   });
@@ -13,13 +18,225 @@ document.addEventListener("DOMContentLoaded", function () {
     boton.addEventListener("click", function () {
       // Obtener el texto del botón que se presionó
       const textoBoton = this.innerText;
+      if (!btncolesterol.classList.contains("d-none")){
+        ocultarbtn();
+      }
       // Utilizar un switch para realizar acciones según el botón presionado
       switch (textoBoton) {
         case "Hemograma":
           header.innerText = "Examen de Hemograma";
+          $(document).ready(function(){
+
+            const labels = ['Recuento']
+            const colors = ['rgb(69,177,223)'];
+            
+          
+            
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label:"Recuento",
+                    data: [5250],
+                    backgroundColor: colors
+                }]
+            };
+            
+            const config = {
+                type: 'bar',
+                data: data,
+                options: {
+                  responsive: true,
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: 'Hemoglobina paciente masculino'
+                    }
+                  },
+                  scales: {
+                    y: {
+                      min: 4750,
+                      max: 5650,
+                    }
+                  }
+                },
+              };
+              if (grafico == null){
+                grafico = new Chart(graph, config)
+              }
+              else{
+                grafico.destroy()
+                grafico = new Chart(graph, config)
+              }
+            
+            
+        });
           break;
         case "Perfíl Lipídico":
           header.innerText = "Examen de Perfíl Lipídico";
+          
+          btncolesterol.classList.remove("d-none")
+          btncolesterol2.classList.remove("d-none")
+          btncolesterol3.classList.remove("d-none")
+          btntrigliceridos.classList.remove("d-none")
+          
+          btncolesterol.addEventListener("click", ()=>{
+            const labels = ['Septiembre','Diciembre','Febrero']
+  const colors = ['#C4EFF3','#7BE7F1','#36E0F0'];
+  
+  const data = {
+      labels: labels,
+      datasets: [{
+          label:"Colesterol HDL en Sangre (mg/dL)",
+          data: [47, 51, 56],
+          backgroundColor: colors
+      }]
+  };
+  
+  const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Colesterol HDL Últimos 3 exámenes'
+          }
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Meses'
+            }
+          },
+          y: {
+            min: 30,
+            max: 65,
+            title: {
+              display: true,
+              text: 'Valores Colesterol HDL en Sangre (mg/dL)'
+            }
+          },
+        }
+      },
+    };
+    if (grafico == null){
+      grafico = new Chart(graph, config)
+    }
+    else{
+      grafico.destroy()
+      grafico = new Chart(graph, config)
+    }
+          })
+          btncolesterol2.addEventListener("click", ()=>{
+            const labels = ['Septiembre','Diciembre','Febrero']
+  const colors = ['#F3230B','#F3422E','#F05341'];
+  
+  const data = {
+      labels: labels,
+      datasets: [{
+          label:"Colesterol LDL en Sangre (mg/dL)",
+          data: [205, 190, 179],
+          backgroundColor: colors
+      }]
+  };
+  
+  const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Colesterol LDL Últimos 3 exámenes'
+          }
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Meses'
+            }
+          },
+          y: {
+            min: 55,
+            max: 220,
+            title: {
+              display: true,
+              text: 'Valores Colesterol LDL en Sangre (mg/dL)'
+            }
+          },
+        }
+      },
+    };
+    if (grafico == null){
+      grafico = new Chart(graph, config)
+    }
+    else{
+      grafico.destroy()
+      grafico = new Chart(graph, config)
+    }
+
+          })
+
+
+  const labels = ['Septiembre','Diciembre','Febrero']
+  const colors = ['#C4EFF3','#7BE7F1','#36E0F0'];
+  
+  
+  
+  const data = {
+      labels: labels,
+      datasets: [{
+          label:"Colesterol HDL en Sangre (mg/dL)",
+          data: [47, 51, 56],
+          backgroundColor: colors
+      }]
+  };
+  
+  const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Colesterol HDL Últimos 3 exámenes'
+          }
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Meses'
+            }
+          },
+          y: {
+            min: 30,
+            max: 65,
+            title: {
+              display: true,
+              text: 'Valores Colesterol HDL en Sangre (mg/dL)'
+            }
+          },
+        }
+      },
+    };
+    if (grafico == null){
+      grafico = new Chart(graph, config)
+    }
+    else{
+      grafico.destroy()
+      grafico = new Chart(graph, config)
+    }
+  
+
+
+
+
           break;
         case "Colesterol":
           header.innerText = "Examen de Colesterol";
@@ -44,3 +261,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+function ocultarbtn(){
+  btncolesterol.classList.add("d-none")
+  btncolesterol2.classList.add("d-none")
+  btncolesterol3.classList.add("d-none")
+  btntrigliceridos.classList.add("d-none")
+}
